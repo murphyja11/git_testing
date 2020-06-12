454a848b745bdf78da5ee66f19480162cbfb64ca
Jack <jack_murphy@brown.edu>
2020-06-12 13:56:41 -0400

    add dog functionality

--- animal.py
+++ animal.py
@@ -1,8 +1,14 @@
 def default():
     print('Hello World')
 
+def dog():
+    print('Woof!')
+
 def main():
-    default()
+    if argv[1] == dog:
+	dog()
+    else: 
+	default()
 
 if __name__ == '__main__':
 	main()
