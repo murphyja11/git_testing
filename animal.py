591c6452e02dfe596a1517794c386448ae019565
Jack <jack_murphy@brown.edu>
2020-06-12 13:52:31 -0400

    add cat functionality

--- animal.py
+++ animal.py
@@ -1,8 +1,14 @@
 def default():
     print('Hello World')
 
+def cat():
+    print('Meow!')
+
 def main():
-    default()
+    if argv[1] == cat:
+	cat()
+    else:
+	default()
 
 if __name__ == '__main__':
 	main()
